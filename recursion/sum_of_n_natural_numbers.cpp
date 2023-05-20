771a01f88f2b8484766ac6704039d57a104fa274
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-05-21 01:30:18 +0530

    Update sum_of_n_natural_numbers.cpp

--- recursion/sum_of_n_natural_numbers.cpp
+++ recursion/sum_of_n_natural_numbers.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int sum(int n){
-    if(n==0){
-        return 0;
+    if(n<=9){       //this slightly improves the time complexity and space complexity since 
+                    //there is one less call (in this case iterative solution is better)
+        return n;
     }
-    return n+sum(n-1);
+
+    return n%10 + sum(n/10);
 }
 
 int main(){
-    cout<<"The sum is:"<<sum(5);
+    int digit;
+    cout<<"Enter digit"<<endl;
+    cin>>digit;
+    cout<<"The sum is:"<<sum(digit)<<endl;
     return 0;
 }
