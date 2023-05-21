3201fe932ae12ff7ace5b8e2a91621d43e8f18a0
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-05-21 15:12:26 +0530

    rope_cutting.cpp

--- recursion/rope_cutting.cpp
+++ recursion/rope_cutting.cpp
@@ -1,3 +1,13 @@
+// Given a rope of length N meters, and the rope can be cut in only 3 sizes A, B and C. The task is to maximizes the number of cuts in rope. 
+//If it is impossible to make cut then print the number else print -1. Examples:
+// Input: 
+// N = 17, A = 10, B = 11, C = 3 
+// Output: 3 
+// Explanation: The maximum cut can be obtain after making 2 cut of length 3 and one cut of length 11. 
+// Input: N = 10, A = 9, B = 7, C = 11 
+// Output: -1 
+// Explanation: It is impossible to make any cut so output will be -1.
+
 #include <iostream>
 #include <cmath>
 using namespace std;
