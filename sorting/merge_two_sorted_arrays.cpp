dc2ca2368e923697c888b0a0797729b1c493e610
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-07-27 20:03:42 +0530

    Create merge_two_sorted_arrays.cpp

---
+++ sorting/merge_two_sorted_arrays.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+/*
+    this sorting algorithm an stable amd inplace algorithm
+    it is used for small arrays
+    it has O(n^2) time complexity in worst case and O(N) time complexity in best case
+    best algorithm for sorting when input size is less
+*/
+
+//sorting two sorted arrays
+void merge_sort(int arr1[],int size1,int arr2[],int size2){
+
+    int n = size1+size2;
+    int sort[n];
+    int i=0;
+    int j =0;
+    for(int k=0;k<n;k++){
+        if(arr1[i]>=arr2[j]){
+            sort[k] = arr2[j];
+            j++;
+        }else{
+            sort[k] = arr1[i];
+            i++;
+        }
+    }
+
+    //printing elements
+    for(int k=0;k<n;k++){   
+        cout<<sort[k]<<" ";
+    }
+}
+
+
+int main(){
+    int arr1[] = {1,5,10,13,15,17};
+    int arr2[] = {2,3,4,8,16,25};
+    merge_sort(arr1,6,arr2,6);
+    return 0;
+}
