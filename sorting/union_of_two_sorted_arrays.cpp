8f4c616b2d701f12102c2649d1dacccb3cd61028
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-07-28 16:48:42 +0530

    Create union_of_two_sorted_arrays.cpp

---
+++ sorting/union_of_two_sorted_arrays.cpp
@@ -0,0 +1,55 @@
+//union of sorted arrays
+#include <iostream>
+#include <cmath>
+using namespace std;
+void merge(int a[],int n1,int b[],int n2){
+
+    int i=0;
+    int j=0;
+    while(i<n1 && j<n2){
+        if(a[i]==a[i+1]){
+            i++;
+            continue;
+        }else if(b[j]==b[j+1]){
+            j++;
+            continue;
+        }else if(a[i]<b[j]){
+            cout<<a[i]<<" ";
+            i++;
+        }else if(b[j]<a[i]){
+            cout<<b[j]<<" ";
+            j++;
+        }else{
+            cout<<a[i]<<" ";
+            i++;
+            j++;
+        }
+    }
+
+    while(i<n1){
+        if(a[i]==a[i+1]){
+            i++;
+            continue;
+        }
+        cout<<a[i]<<" ";
+        i++;
+    }
+
+    while(j<n2){
+        if(b[j]==b[j+1]){
+            j++;
+            continue;
+        }
+        cout<<b[j]<<" ";
+        j++;
+    }
+    
+}
+
+
+int main(){
+    int a[] = {3,5,8};
+    int b[] = {2,3,8,9,10,15};
+    merge(a,3,b,6);
+    return 0;
+}
