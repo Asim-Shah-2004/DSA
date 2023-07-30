4f50f164e091b082f403e5f94b4b43cc3eb29207
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-07-30 22:27:14 +0530

    Create sorting_two_types_of_elements.cpp

---
+++ sorting/sorting_two_types_of_elements.cpp
@@ -0,0 +1,26 @@
+//use modification of lamuto partition
+#include <iostream>
+using namespace std;
+
+void print(int a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+//seperating odd and even
+void seperete(int a[],int n){
+    int i=-1;
+    for(int j=0;j<n;j++){
+        if(a[j]%2==0){
+            i++;
+            swap(a[i],a[j]);
+        }
+    }
+    print(a,n);
+}
+
+int main(){
+    int a[]={1,2,3,4,5,6,7};
+    seperete(a,7);   
+}
