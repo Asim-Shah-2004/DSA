44fd6f29e2d69bf7657b60328e270a1dcfb09bf3
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-04 22:21:41 +0530

    Update sum_of_array.cpp

--- hashing/sum_of_array.cpp
+++ hashing/sum_of_array.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 bool sum(int arr[],int n,int sum){
-    unordered_set <int> s(arr,arr+n);
+    unordered_set <int> s;
     for(int i=0;i<n;i++){
         if(s.count(sum-arr[i])){
             return true;
+        }else{
+            s.insert(arr[i]);
         }
     }
-
     return false;
 }
 
 int main(){
-    int arr[] = {1,5,6,7,2,3};
-    if(sum(arr,6,12)){
+    int arr[] = {8,3,4,22,5};
+    if(sum(arr,5,12)){
         cout<<"sum exists "<<endl;
     }else{
         cout<<"sum dosent exists "<<endl;
