36d58966f5606e392217e9decd851620b5df8b94
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-09 19:10:30 +0530

    Create delete_doubly_linked_list_at_head.cpp

---
+++ linked_list/delete_doubly_linked_list_at_head.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* prev;
+    node(int data){
+        this->data=data;
+        next=NULL;
+        prev=NULL;
+    }
+};
+
+
+void insertAtTail(node* &head,int data){
+    node* n = new node(data);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void delete_at_head(node* &head){
+    node* temp = head;
+    head=head->next;
+    head->prev=NULL;
+    delete temp;
+}
+
+void display(node* &head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    node* head = NULL;
+    for(int i=1;i<=10;i++){
+        insertAtTail(head,i);
+    }
+    display(head);
+    delete_at_head(head);
+    display(head);
+    return 0;
+}
