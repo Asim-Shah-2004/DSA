2274f55a9696685afeddf64a1730458a82f5c01a
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-09 20:26:04 +0530

    Create delete_at_tail_in_doubly_linked_list.cpp

---
+++ linked_list/delete_at_tail_in_doubly_linked_list.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* prev;
+    node(int data){
+        this->data=data;
+        next=NULL;
+        prev=NULL;
+    }
+};
+
+
+void insertAtTail(node* &head,int data){
+    node* n = new node(data);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void delete_at_tail(node* &head){
+    if(head==NULL){
+        return;
+    }else if(head->next==NULL){
+        head=NULL;
+        return;
+    }
+    node* temp = head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    node* temp2 = temp->next;
+    temp->next=NULL;
+    delete temp2;
+}
+
+void display(node* &head){
+    if(head==NULL){
+        return;
+    }
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    node* head = NULL;
+    for(int i=0;i<=10;i++){
+        insertAtTail(head,i);
+    }
+    display(head);
+    delete_at_tail(head);
+    display(head);
+    return 0;
+}
