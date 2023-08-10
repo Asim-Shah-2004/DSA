49434483a9477a028ef8485a0fa8ecc079e2608e
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-10 17:16:03 +0530

    Create circular_linked_list_implimentation.cpp

---
+++ linked_list/circular_linked_list_implimentation.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+        node(int data){
+            this->data=data;
+            next=NULL;
+        }
+};
+
+void create(node* &head,int arr[],int size){
+
+    node* n = new node(arr[0]);
+    if(head==NULL){
+        head=n;
+    }
+    node* temp = head;
+
+    for(int i=1;i<size;i++){
+        node* n = new node(arr[i]);
+        temp->next=n;
+        temp=temp->next;
+    }
+
+    temp->next=head;
+}
+
+void display(node* &head){
+    if(head==NULL){
+        cout<<"create a linked list"<<endl;
+        return;
+    }
+    cout<<head->data<<" ";
+    for(node*temp=head->next;temp!=head;temp=temp->next){
+        cout<<temp->data<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int data[] = {20,50,60,30,40};
+    node* head = NULL;
+    create(head,data,5);
+    display(head);
+    return 0;
+}
