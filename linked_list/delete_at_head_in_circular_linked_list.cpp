cebe0977acc074fca9543b49d5358a04d124b762
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-11 23:37:12 +0530

    Create delete_at_head_in_circular_linked_list.cpp

---
+++ linked_list/delete_at_head_in_circular_linked_list.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+        node(int data){
+            this->data=data;
+            next=NULL;
+        }
+};
+
+void create(node* &head,int arr[],int size){
+
+    node* n = new node(arr[0]);
+    if(head==NULL){
+        head=n;
+    }
+    node* temp = head;
+
+    for(int i=1;i<size;i++){
+        node* n = new node(arr[i]);
+        temp->next=n;
+        temp=temp->next;
+    }
+
+    temp->next=head;
+}
+
+void display(node* &head){
+    if(head==NULL){
+        cout<<"create a linked list"<<endl;
+        return;
+    }
+    cout<<head->data<<" ";
+    for(node*temp=head->next;temp!=head;temp=temp->next){
+        cout<<temp->data<<" ";
+    }
+    cout<<endl;
+}
+
+void insertAtEnd(node* &head,int data){
+    node* n = new node(data);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    n->next=head->next;
+    head->next=n;
+    swap(n->data,head->data);
+    head=head->next;
+}
+
+void deleteAtHead(node* &head){
+    node* temp = head;
+    node* temp2 = head;
+    while(temp->next!=head){
+        temp=temp->next;
+    }
+
+    head=head->next;
+    temp->next=temp2->next;
+    delete temp2;
+
+}
+
+
+int main(){
+    int data[] = {20,50,60,30,40};
+    node* head = NULL;
+    create(head,data,5);
+    display(head);
+    deleteAtHead(head);
+    display(head);
+    return 0;
+}
