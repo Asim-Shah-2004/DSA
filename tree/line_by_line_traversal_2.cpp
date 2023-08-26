0af57b19e658e2b776687c83571e5809bd6d20b5
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-26 16:10:54 +0530

    Create line_by_line_traversal_2.cpp

---
+++ tree/line_by_line_traversal_2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+void print(Node* root){
+    queue <Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        int count = q.size();
+        for(int i=0;i<count;i++){
+            Node* curr = q.front();
+            q.pop();
+            cout<<curr->data<<" ";
+            if(curr->left!=NULL){
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                q.push(curr->right);
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+int main() {
+
+    Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->right=new Node(70);
+	root->right->right->right=new Node(80);
+	
+	print(root);
+}
