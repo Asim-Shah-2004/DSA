7cbbfdf5d53e576a034c40f49f48500f2d7236eb
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-08-26 15:48:31 +0530

    Create level_order_traversal.cpp

---
+++ tree/level_order_traversal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+void print(Node* root){
+    queue <Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* curr = q.front();
+        cout<<curr->data<<" ";
+        q.pop();
+        if(curr->left!=NULL){
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL){
+            q.push(curr->right);
+        }
+    }
+}
+
+
+int main() {
+
+    Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->right=new Node(70);
+	root->right->right->right=new Node(80);
+	
+	print(root);
+}
