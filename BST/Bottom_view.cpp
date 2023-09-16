25490515828c9a775b2381d1409d3203449131a5
Asim-Shah-2004 <123801234+Asim-Shah-2004@users.noreply.github.com>
2023-09-16 20:13:09 +0530

    Create Bottom_view.cpp

---
+++ BST/Bottom_view.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left;
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+void Bottom_view(Node *root){
+    map<int,int> mp;
+    queue<pair<Node*,int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        Node* curr = it.first;
+        int hd = it.second;
+        mp[hd]=curr->key;
+        if(curr->left!=NULL){
+            q.push({curr->left,hd-1});
+        }
+        if(curr->right!=NULL){
+            q.push({curr->right,hd+1});
+        }
+    }
+    for(auto x:mp){
+        cout<<x.second<<" ";
+    }
+}
+
+int main() {
+	
+	Node *root = new Node(10);  
+    root->left = new Node(20);  
+    root->right = new Node(30);  
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    
+    Bottom_view(root);
+          
+    return 0;  
+	
+}
